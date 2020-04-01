95138f4ae21f073c390495e6cb32ad1d8ad86a84
Rudnev Sergey <rudnev090@gmail.com>
2020-04-01 16:09:50 +0300

    modalka

--- scrirt.js
+++ scrirt.js
@@ -8,12 +8,14 @@ HamburgerMenuLink.addEventListener('click', function(e) {
   e.preventDefault();
 
   HamburgerMenu.style.display = 'block';
+  body.style.overflow = 'hidden';
 });
 
 HamburgerMenuClose.addEventListener('click', function(e) {
   e.preventDefault();
 
   HamburgerMenu.style.display = 'none';
+  body.style.overflow = 'visible';
 });
 
 // AccordeonTeam
